f1fb4827c11691a25481ee869160b6a9d9a1a91b
Шороховы <soroho78@gmail.com>
2025-01-17 23:09:52 +0300

    homework12

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,11 @@
 public class Book {
     private String nameBook;
     private int year;
-    private String Author;
-    public Book(String nameBook, int year, String author){
+    private Author author;
+    public Book(String nameBook, int year, Author author){
         this.nameBook = nameBook;
         this.year = year;
-        this.Author = author;
+        this.author = author;
     }
     public String getNameBook(){
         return this.nameBook;
@@ -13,8 +13,8 @@ public class Book {
     public int getYear(){
         return this.year;
     }
-    public String getAuthor(){
-        return this.Author;
+    public Author getAuthor(){
+        return this.author;
     }
     public  void setYear(int year){
         this.year = year;
